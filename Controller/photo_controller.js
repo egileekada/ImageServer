8fd3230d77bc38a4d02e07fb9b75d131b880ac45
egileekada <egileoniso.ekada@gmail.com>
2021-06-09 15:20:58 +0100

    updating connection

--- Controller/photo_controller.js
+++ Controller/photo_controller.js
@@ -1,46 +1,39 @@
-const express = require("express"); 
+const express = require("express");  
 const router = express.Router();  
 const upload = require("../Routers/upload")
 const update = require("../Routers/update") 
 const remove = require("../Routers/delete")  
-const database = require('../Database/database') 
+const database = require('../Database/database')  
+const assert = require('assert');
 
-const client = database.connect 
+const client = database.connect  
 
 async function findImageByName(client, name, res){ 
- 
-    const cursor = await client.db("Photo_Storage").collection("data").find({
-        name: { $regex: name, $options:'i' }
-    });
 
-    const result = await cursor.toArray(); 
-    res.json(result)
+    client.connect( async function(err) {  
+        if(err) return err
+        const db = client.db("Photo_Storage");
+        db.collection("data").find({ name: { $regex: name, $options:'i' } }).toArray(function(err, docs) {
+            assert.equal(err, null); 
+            res.json(docs)   
+          }); 
+    })
+ 
+    // const cursor = await client.db("Photo_Storage").collection("data").find({
+    //     name: { $regex: name, $options:'i' }
+    // });
+
+    // const result = await cursor.toArray(); 
+    // res.json(result)
 }
 
 router.use("/image", upload, remove, update);   
 
-router.post("/view_image", async (req, res)=>{ 
+router.post("/view_image", async (req, res) => { 
     try {    
         let name = req.body.data
         await findImageByName(client, name, res);  
         
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ err: 'Something went wrong' });
-    } finally {
-        client.close();
-    }
-}); 
-
-router.get("/search", async (req, res)=>{ 
-    try {   
-        let name = req.query.name  
-        const cursor = client.db("Photo_Storage").collection("data").find({
-            name: { $regex: name, $options:'i' }
-        });
-
-        const result = await cursor.toArray(); 
-        res.json(result)         
     } catch (err) {
         console.error(err);
         res.status(500).json({ err: 'Something went wrong' });
@@ -50,17 +43,50 @@ router.get("/search", async (req, res)=>{
     // }
 }); 
 
-router.get("/viewall", async (req, res)=>{ 
-    try {      
-        const cursor = client.db("Photo_Storage").collection("data").find();
+router.get("/search", async (req, res)=>{ 
 
-        const results = await cursor.toArray(); 
-        res.json(results)  
+    let name = req.query.name 
+    client.connect( async function(err) {  
+        if(err) return err
+        const db = client.db("Photo_Storage");
+        db.collection("data").find({ name: { $regex: name, $options:'i' } }).toArray(function(err, docs) {
+            assert.equal(err, null); 
+            res.json(docs)   
+          }); 
+    })
+    // try {   
+    //     let name = req.query.name  
+    //     const cursor = client.db("Photo_Storage").collection("data").find({
+    //         name: { $regex: name, $options:'i' }
+    //     });
+
+    //     const result = await cursor.toArray(); 
+    //     res.json(result)         
+    // } catch (err) {
+    //     console.error(err);
+    //     res.status(500).json({ err: 'Something went wrong' });
+    // }  
+}); 
+
+router.get("/viewall",async (req, res)=>{   
+    client.connect( async function(err) {  
+        if(err) return err
+        const db = client.db("Photo_Storage");
+        db.collection("data").find().toArray(function(err, docs) {
+            assert.equal(err, null); 
+            res.json(docs)   
+          }); 
+    })
+    // try {      
+    //     const cursor = client.db("Photo_Storage").collection("data").find();
+
+    //     const results = await cursor.toArray(); 
+    //     res.json(results)   
         
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ err: 'Something went wrong' });
-    } 
+    // } catch (err) {
+    //     console.error(err);
+    //     res.status(500).json({ err: 'Something went wrong' });
+    // } 
     // finally {
     //     client.close();
     // }
--- Routers/delete.js
+++ Routers/delete.js
@@ -6,10 +6,15 @@ const database = require('../Database/database')
 const client = database.connect 
 
 router.post("/delete", async (req, res)=>{ 
-    try {      
-        await client.db("Photo_Storage").collection("data").deleteOne(
-            { name: req.body.data.name }
-        );
+    try {       
+        client.connect( async function(err) {  
+            if(err) return err
+            const db = client.db("Photo_Storage");
+            db.collection("data").deleteOne({ name: req.body.data.name })
+        })
+        // await client.db("Photo_Storage").collection("data").deleteOne(
+        //     { name: req.body.data.name }
+        // );
         console.log("A File Named "+ req.body.data.name +" has been deleted") 
         res.json({ msg: 'yaya' });  
         
