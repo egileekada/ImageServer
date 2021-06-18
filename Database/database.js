67518d25965040cdf40357189b5619ff95eb3906
egileekada <egileoniso.ekada@gmail.com>
2021-06-18 02:21:39 +0100

    updating connection

--- Database/database.js
+++ Database/database.js
@@ -1,19 +1,19 @@
 const {MongoClient} = require("mongodb");  
 
-require('dotenv').config();
-const client = new MongoClient(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 5000, socketTimeoutMS: 5000} );
+// require('dotenv').config();
+// const client = new MongoClient(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 5000, socketTimeoutMS: 5000} );
 
-    async function main() { 
-        try {
-            await client.connect();  
-        } catch (e) {
-            console.log(e);
-        }
-    } 
+//     async function main() { 
+//         try {
+//             await client.connect();  
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     } 
     
-    module.exports = {
-        connect: client  
-    } 
+//     module.exports = {
+//         connect: client  
+//     } 
 
-    main()
+//     main()
       
